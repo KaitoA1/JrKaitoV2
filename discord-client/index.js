8267aa06ab08e2fb6ad922d0efa5f259f86dc32d
Matthew <63630063+KaitoA1@users.noreply.github.com>
2020-05-18 00:54:37 -0700

    Alpha v1.0.6

--- discord-client/index.js
+++ discord-client/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 
 const bot = new Client();
+
+['commands', 'aliases'].forEach(c => bot[c] = new Collection());
+['commandHandler', 'eventHandler'].forEach(h => require(`./utils/handlers/${h}`)(bot));
 
 bot.login(process.env.botTOKEN);
